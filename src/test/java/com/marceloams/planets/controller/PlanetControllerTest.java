74a76e68faca5a7d43481e6530f946f60b1080f5
Marcelo Amorim <mamorim813@gmail.com>
2024-01-28 00:26:56 -0300

    Created test for PlanetController class with getAll, getById and getByName tests.

---
+++ src/test/java/com/marceloams/planets/controller/PlanetControllerTest.java
@@ -0,0 +1,65 @@
+package com.marceloams.planets.controller;
+
+import com.marceloams.planets.PlanetsApplication;
+import com.marceloams.planets.dto.PlanetDTO;
+import com.marceloams.planets.model.Planet;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PlanetsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PlanetControllerTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    private final String baseRequestUrl = "/api/v1/planets";
+
+    private ResponseEntity<List<PlanetDTO>> getPlanets(){
+        return restTemplate.exchange(
+                baseRequestUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<PlanetDTO>>() {}
+        );
+    }
+
+    private ResponseEntity<PlanetDTO> getPlanet(String url){
+        return restTemplate.getForEntity(baseRequestUrl+url, PlanetDTO.class);
+    }
+
+    @Test
+    public void getAll(){
+        List<PlanetDTO> planetDTOS = getPlanets().getBody();
+        Assertions.assertNotNull(planetDTOS);
+        Assertions.assertEquals(2, planetDTOS.size());
+    }
+
+    @Test
+    public void getById(){
+        PlanetDTO planetDTO = getPlanet("/1").getBody();
+        Assertions.assertNotNull(planetDTO);
+        Assertions.assertEquals(1, planetDTO.getId());
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, getPlanet("/0").getStatusCode());
+    }
+
+    @Test
+    public void getByName(){
+        PlanetDTO planetDTO = getPlanet("/name/Tattoine").getBody();
+        Assertions.assertNotNull(planetDTO);
+        Assertions.assertEquals("Tattoine", planetDTO.getName());
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, getPlanet("/name/Non-existent").getStatusCode());
+    }
+}
